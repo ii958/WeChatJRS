5562377c3cda737eeae0c8ca42e3626e97856c07
donkey <476707501@qq.com>
2017-04-07 15:42:49 +0800

    左边栏事件
    增加了左边栏的显示和隐藏的函数，以及主题列表的加载

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,7 +12,7 @@ Page({
     currentDateStr: '',
     currentDate: new Date(),
     refreshAnimation: {},//加载更多旋转动画数据
-    loadingMode: false,//是否正在加载
+    loadingMore: false,//是否正在加载
     avatarUrl: '',//当前开发者头像
     nickName: '',//当前开发者名字
 
@@ -79,11 +79,11 @@ Page({
   onReady: function(){
     var date = utils.getCurrentDate();
     this.setData({
-      loading:true
+      currentDateStr: date.year+'.'+date.month+'.'+date.day+' ' + '星期' + weekdayStr[date.weekday]
     });
 
     var that = this;
-
+    that.setData({loading:true});
     requests.getNewsLatest((data) => {
       data = utils.correctData(data);
       that.setData({
@@ -99,5 +99,115 @@ Page({
     requests.getTheme((data) => {
       that.setData({themeData: data.others});
     });
+  },
+
+  //列表加载更多
+  loadingMoreEvent: function(e){
+    if(this.data.loadingMore)
+      return;
+    console.log(this.date.currentDate);
+    var date = new Date(Date.parse(this.date.currentDate) - 1000 * 60 * 60 * 24);
+    var that = this;
+    var pageData = [];
+
+    this.setData({loadingMore: true});
+    updateRefreshIcon.call(this);
+
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    m = m>9 ? m : '0'+m;
+    d = d>9 ? d : '0'+d;
+    var dateStr = [y,m,d].join('');
+    requests.getBeforeNews(dateStr, (data) => {
+      data = utils.correctData(data);
+      console.log(data);
+      pageData = that.data.pageData;
+      pageData.push({type:'3',title:([y,m,d].join('.') + ' 星期' + weekdayStr[date.getDay()])});
+      pageData = pageData.concat(data.stories);
+
+      that.setData({currentDate:date,pageData:pageData});
+    }, null, () => {
+      that.setData({loadingMore:false});
+    });
+  },
+
+  //浮动球移动事件
+  ballMoveEvent: function(e){
+    var touchs = e.touches[0];
+    var pageX = touchs.pageX;
+    var pageY = touchs.pageY;
+    if(pageX < 25) return;
+    if(pageX > this.data.screenWidth - 25) return;
+    if(this.data.screenHeight - pageY <= 25) return;
+    if(pageY<=25) return;
+    var x = this.data.screenWidth - pageX - 25;
+    var y = this.data.screenHeight - pageY - 25;
+    this.setData({
+      ballBottom: y,
+      ballRight: x
+    });
+  },
+
+  //浮动球点击 侧栏展开
+  ballClickEvent: function(){
+    slideUp.call(this);
+  },
+  //遮罩点击 侧栏关闭
+  slideCloseEvent: function(){
+    slideDown.call(this);
+  },
+  authorShowEvent: function(){
+    this.setData({modalMsgHidden: false});
+  },
+  modalMsgHiddenEvent: function(){
+    this.setData({modalMsgHidden: true});
+  },
+  onPullDownRefresh: function(e){
+    console.log(1);
   }
-})
+});
+
+//侧栏展开
+function slideUp(){
+  var animation = wx.createAnimation({
+    duration: 600
+  });
+  this.setData({maskDisplay: 'block'});
+  animation.translateX('100%').step();
+  this.setData({
+    slideAnimation: animation.export()
+  });
+}
+
+//侧栏关闭
+function slideDown(){
+  var animation = wx.createAnimation({
+    duration: 800
+  });
+  animation.translateX('-100%').step();
+  this.setData({slideAnimation:animation.export()});
+  this.setData({maskDisplay:'none'});
+}
+
+/**
+ * 旋转上拉加载图标
+ */
+function updateRefreshIcon(){
+  var deg = 360;
+  var that = this;
+
+  var animation = wx.createAnimation({
+    duration: 1000
+  });
+
+  var timer = setInterval(function(){
+    if(!that.data.loadingMore)
+      clearInterval(timer);
+    animation.rotateZ(deg).step();
+    deg += 360;
+    that.setData({
+      refreshAnimation: animation.export()
+    })
+  }, 1000);
+}
