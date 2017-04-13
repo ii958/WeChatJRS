10f62d62414b05958e149f05dfcada32815621c8
donkey <476707501@qq.com>
2017-04-13 10:39:33 +0800

    详细页面的加载以及图片预览

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -9,6 +9,8 @@ Page({
     isTheme: false,
     news: {},//日报详情
     modalHidden: true,
+    extraInfo: {},
+    modalMsgHidden: true,
     pageShow: 'none',
     isCollect: false//是否被收藏
   },
@@ -39,6 +41,62 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  reloadEvent: function(){
+    loadData.call(this);
+  },
+  showModalEvent: function(){
+    this.setData({modalHidden:false});
+  },
+  hideModalEvent: function(){
+    this.setData({modalHidden: true});
+  },
+  //现在图片预览不支持调试显示，看不到效果
+  //图片预览【当前是当前图片，以后会考虑整篇日报的图片预览】
+  previewImgEvent: function(e){
+    var src = e.currentTarget.dataset.src;
+    if(src && src.length > 0){
+      wx.previewImage({
+        // current: 'String', // 当前显示图片的链接，不填则默认为 urls 的第一张
+        urls: [src]
+      });
+    }
+  },
+  //跳转到评论页面
+  toCommentPage: function(e){
+    var storyId = e.currentTarget.dataset.id;
+    var longCommentCount = this.data.extraInfo ? this.data.extraInfo.long_comments : 0;//长评论数
+    var shortCommnetCount = this.data.extraInfo ? this.data.extraInfo.short_comments : 0;//短评论数
+    //跳转到评论页面，并传递评论数目信息
+    wx.navigateTo({
+      url: '../comment/comment?lcount=' + longCommentCount + '&scount=' + shortCommnetCount + '&id=' + storyId
+    });
+  },
+  //收藏
+  collectOrNot: function(){
+    var pageData = wx.getStorageSync('pageData') || []
+    console.log(pageData);
+    if(this.data.isCollect){
+      for(let i=0;i<pageData.length;i++){
+        if(pageData[i].id == this.data.id){
+          pageData.splice(i,1);
+          this.setData({isCollect: false});
+          break;
+        }
+      }
+    }else{
+      var images = new Array(this.data.news.image);
+      var item = {id : this.data.id, title: this.data.news.title, images : images};
+      console.log(item);
+      pageData.unshift(item);
+      this.setData({isCollect:true});
+    }
+    try{
+      wx.setStorageSync('pageData', pageData);
+    }catch(e){
+
+    }
+    console.log(pageData);
   }
 });
 
